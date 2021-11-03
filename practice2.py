b048cf610b2e798461e320731dcbf913efd04d2c
JJIMINSHIN <86641936+JJIMINSHIN@users.noreply.github.com>
2021-11-03 23:18:36 +0900

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,19 @@
+#201
+def print_coin():
+    print("비트코인")
+
+#202
+print(print_coin)
+
+#203
+
+for i in range(100):
+   print_coin()
+
+#204
+def print_coins():
+    print("비트코인")
+
+    for  i in range(100):
+        print_coins
+
