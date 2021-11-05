bdad90aec6e5665fd698065ede2bc3dccb75818a
JJIMINSHIN <scalla1031@gmail.com>
2021-11-05 22:49:14 +0900

    initial commit

---
+++ pra/211220.py
@@ -0,0 +1,49 @@
+#211
+# 안녕
+# HI
+
+#212 
+def amex(a,b):
+    print(a+b)
+
+amex(3,4)
+amex(4,7)
+#7, 11
+
+#213 함수호출 X
+#214 타입에러
+#215
+def print_with_smile(string):
+    print(string+":D")
+
+#216
+
+print_with_smile("안녕하세요")
+
+#217 
+def print_uppper_prince(price):
+    print(price*1.3)
+
+#218
+def print_sum(a,b):
+    print(a+b)
+
+#219
+
+def print_arithmetic_operation(3,4):
+    print(a, "+", b, "=", a + b)
+    print(a, "-", b, "=", a - b)
+    print(a, "*", b, "=", a * b)
+    print(a, "/", b, "=", a / b)
+
+#220
+
+def print_max(a,b,c):
+    max =0
+    if a> max  :
+        return a
+    elif b> max :
+        return b
+    if c > max :
+        return c
+    print(max)
