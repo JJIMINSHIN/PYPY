9f7f1923452e9eb516d0ee9df2f63ff17587a87e
JJIMINSHIN <scalla1031@gmail.com>
2022-01-26 22:06:34 +0900

    카드정병의 장미세기

---
+++ elice/elice_java2.java
@@ -0,0 +1,26 @@
+package elice;
+
+public class Main {
+	public static void main(String args[]) {
+        // ?를 채워주세요
+        int x = 6;
+        int y = 3;
+
+        // 빈 곳도 채워주세요!
+        // 더하기 연산
+        int my_sum = x+y;
+        System.out.println("my_sum : " + my_sum);
+
+        // 빼기 연산
+        int my_sub = x-y;
+        System.out.println("my_sub : " + my_sub);
+
+        // 곱하기 연산
+        int my_mul = x*y;
+        System.out.println("my_mul : " + my_mul);
+
+        // 나누기 연산
+        int my_div = x/y;
+        System.out.println("my_div : " + my_div);
+	}
+}
