76124ecb2a3bb55eb66c399dc1f21c84ea96290c
JJIMINSHIN <scalla1031@gmail.com>
2022-01-29 21:29:52 +0900

    거꾸로 숫자세기!

---
+++ elice/elice_java3.java
@@ -0,0 +1,12 @@
+package elice;
+
+public class Main {
+	public static void main(String args[]) {
+        int count = 10;
+        for ( count = 10;  count >= 1; count--)
+        {
+         System.out.println("현재 숫자: "+ count);
+        }
+        
+	}
+}
